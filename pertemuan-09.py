5f3282cea9e9ccbe95e2c7e242592422920d94ff
Anung B Ariwibowo <barliant@gmail.com>
2024-11-01 10:33:58 +0700

    Modify pertemuan-09.py untuk contoh deskripsi tugas kelompok.

--- pertemuan-09.py
+++ pertemuan-09.py
@@ -2,36 +2,61 @@ import streamlit as st
 
 # Ini bagian heading aplikasi Streamlit
 st.title("Kuliah Praktikum Big Data")
-st.write("Anung")
-st.write("# Heading 1")
-st.write('#### Heading 4')
-st.write('###### Heading 6')
+st.write("# Pendahuluan")
+st.write("Contoh aplikasi `streamlit` untuk proyek kelompok mata kuliah Praktikum Big Data.")
+st.write("## Petunjuk Proyek Kelompok")
+str_petunjuk = '''
+- Proyek Kelompok dibuat menggunakan `streamlit`.
+- Harus menggunakan dataset yang diambil dari sumber data seperti Kaggle, BPS, dan sebagainya.
+- Harus ada visualisasi dalam bentuk grafis.
+- Visualiasi harus bisa dianalisis secara dinamis menggunakan variabel dari elemen-elemen `streamlit`.
+- Ada analisis sederhana terhadap visualisasi grafis tersebut.
+- Ada simpulan terhadap analisis.
+'''
+st.write(str_petunjuk)
 
-# Kinerja unit
-st.metric("Kinerja", 40, -1)
-st.metric("Response Time", 30, 20)
-st.metric("Saham", 100, 20)
+st.write("# Contoh-contoh elemen `streamlit`")
+
+st.write("## Button")
+btn_contoh = st.button("Coba klik saya")
+st.write(f"Tombol sudah diklik: {btn_contoh}")
 
 # Pilihan
+st.write('## Checkbox')
 pilih1 = st.checkbox('Ya')
 pilih2 = st.checkbox('Tidak')
 
-st.write(pilih1)
-st.write(pilih2)
+st.write(f"Ya: {pilih1} Tidak: {pilih2}.")
 
-"""
-Ini komentar harusnya
-"""
+st.write("## Toggle")
+saklar = st.toggle("Activate")
+st.write(f"{saklar}")
 
-makanan = st.radio('Makanan kesukaan', ['Bakso', 'Nasi goreng', 'Mie ayam'] )
+st.write("## Radiobox")
+makanan = st.radio('Apa makanan kesukaanmu?', ['Bakso', 'Nasi goreng', 'Mie ayam', 'Yang lain'] )
+st.write(f"Makanan kesukaanmu adalah: {makanan}.")
 
-st.write(makanan)
-
+st.write("## Selection Box")
 minuman = st.selectbox('Pilih minuman yang akan dipesan',
             ['Es teh', 'Kopi', 'Jus'] )
 
 st.write(minuman)
 
+st.write("## Multiple Selection")
 bayar = st.multiselect('Bayar pakai:',
                       ['Tunai', 'Ovo', 'GoPay'])
 st.write(bayar)
+
+st.write("## Slider")
+volume = st.select_slider("Volume suara", list(range(1,11)))
+st.write(f"Volume: {volume}")
+
+st.write("## Metric")
+# Kinerja unit
+st.metric("Kinerja", 40, -1)
+st.metric("Response Time", 30, 20)
+st.metric("Saham", 100, 20)
+
+st.write("## `streamlit` widgets")
+st.write("Untuk contoh-contoh widget yang lain, bisa dilihat di [`streamlit` widgets](https://docs.streamlit.io/develop/api-reference/widgets).")
+
